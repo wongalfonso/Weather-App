015140fb63c92cebfdeac577cdd31763b872294c
wongalofonso <alfonsowongsd@gmail.com>
2018-04-15 19:43:52 -0700

    fixed

--- src/js/index.jsx
+++ src/js/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from "react-redux";
 import App from './app';
-import store from "./RootStore";
+import store from "./rootStore";
 
 render(
     <Provider store={ store }>
